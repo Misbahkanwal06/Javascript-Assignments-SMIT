5e1cf3a185b8849514ff0356d47511352638f950
Misbahkanwal06 <125421589+Misbahkanwal06@users.noreply.github.com>
2023-05-04 09:28:10 -0700

    Add files via upload
    Assignment-01

---
+++ JS Assignment1/app.js
@@ -0,0 +1,26 @@
+ // QUESTION #1.
+//  alert('Javascript Alerts! \n\n\n Assalam-u-alaikum');
+ // OR
+//  let greet = "Assalam u alaikum!";
+//  alert(greet);
+
+ // QUESTION #2.
+//  alert('Javascript Alerts! \n\n\n Error! Please enter a valid password');
+ // OR
+//  let password = "Error!Please enter a valid password";
+//  alert(password);
+
+ // QUESTION #3.
+// alert('Javascript Alerts! \n\n\n Welcome to JS Land... \n Happy Coding!'); 
+// OR 
+//  let greeting = "Welcome to Js Land... \n Happy Coding!";
+//  alert(greeting);
+
+ //QUESTION #4
+// alert('Javascript Alerts! \n\n\n  Welcome to JS land');
+// alert('Happy Coding ! \n prevent this page from creating additional dialogs.');
+
+// Question #5
+// alert('Javascript Alerts! \n\n\n  Hello.. I can run JS through my web browsers Console');   
+
+  

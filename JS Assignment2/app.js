bf24a9c0bd2673ca0d51d42503edea887be7a654
Misbahkanwal06 <125421589+Misbahkanwal06@users.noreply.github.com>
2023-05-04 09:29:02 -0700

    Add files via upload
    Assignment-02

---
+++ JS Assignment2/app.js
@@ -0,0 +1,50 @@
+//  QUESTION #1 DECLARE A VARIABLE CALLED USERNAME:
+// var userName; 
+
+// QUESTION #2 Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// var myName = "Misbah kanwal";
+
+// QUESTION #3  // Write script to  // a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message // c) Display the message in alert box.
+
+// var message;
+// message = "Hello world"; 
+// alert(message);
+
+//  QUESTION #4 Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// var stuName = "Student Name: \n\n Misbah kanwal";
+// alert(stuName);
+// var stuRollNo = "Student's Roll-No \n\n 107";
+// alert(stuRollNo);
+// var course = "Student Enrolled In:\n\nWeb and Mobile app Development";
+// alert(course);
+
+// QUESTION #5 Write a script to display the following alert using one JS  variable:
+// var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza);
+
+// QUESTION #6 Declare a variable called email and assign to it a string that represents your
+//  Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.
+//  (Hint: usestring concatenation).
+
+// var email = "misbahkan26@gmail.com";
+// alert('My Email is ' + email);
+
+// QUESTION #7 Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+//  Display the following message in an alert box:
+// var book = "A smarter way to learn Javascript";
+// alert('I am learning from the book ' + book);
+
+// QUESTION #8 Write a script to display this in browser through JS
+// 
+//document.write("Yah! I can write HTML content through JS");
+//OR
+//  let write  = prompt("write your ans \n");
+// document.write(write + "I can write HTML content through JS");
+
+//QUESTION #9 //Store following string in a variable and show in alert and browser through JS.
+// let pattern =
+// alert("\n\n“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+

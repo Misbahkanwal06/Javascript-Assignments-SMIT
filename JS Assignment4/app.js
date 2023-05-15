007dd5d648954dc813672ba077a19792ae1f6dbc
Misbahkanwal06 <125421589+Misbahkanwal06@users.noreply.github.com>
2023-05-15 12:23:50 -0700

    Assignment 04

---
+++ JS Assignment4/app.js
@@ -0,0 +1,21 @@
+//Question1: Declare 3 variables in one statement.
+var name; var num; var cnic;
+
+//Question2: Declare 5 legal & 5 illegal variable names.
+//Legal variables
+var _name; var $name; var name$; var $2name; var name2$;
+//Illegal variables
+//var 2name; var @name; var first name; var %name; var *name;
+
+//Question3:. Display this in your browser
+//a) A heading stating “Rules for naming JS variables”
+document.write("Rules for naming JS variables");
+//b) Variable names can only contain ______, ______, For example $my_1stVariable
+document.write("Variable names can only contain $ and _ , for example $my_name ");
+
+//c) Variables must begin with a ______, ______  For example $name, _name or name
+document.write("variable must begin with _name or $name");
+// d) Variable names are case _________
+document.write("variable names are case sensitive");
+// e) Variable names should not be JS _________
+document.write("variable names should not be JS keywords");

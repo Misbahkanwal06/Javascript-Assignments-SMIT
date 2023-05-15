29c572787b0de51c69bada6d4a57d2fcb2478e85
Misbahkanwal06 <125421589+Misbahkanwal06@users.noreply.github.com>
2023-05-15 12:22:19 -0700

    Assignment03

---
+++ JS Assignment3/app.js
@@ -0,0 +1,20 @@
+// Question#1: Declare a variable called age and assign it your age and show your result in alert box?
+
+var age = 20;
+alert( "I'm "+ age + "  years old" );
+
+// Question#2 : Declare and initialize a variable to keep track of how many times a visitor
+//has visited a  web page.show his/her number of visits on yourweb page?
+var num = 5;
+alert("You have visited this site "+ num + " times");
+
+//Question #3: Declare a variable called birth year. and assign to it your birth year.shoe the following message in your browser
+var birthYear =  2001;
+document.write("My birth year is " + birthYear+ "\n Data type of the declared variable is number"  );
+
+//Question#4:  A visitor visites an online clothing store . write a script to store in variables
+//the folowing information. visitor's name, product title and quantity i.e (how many products a visitor wants t order)
+var name = "jhon doe ";
+var productTitle = "T-shirts";
+var quantity = 5;
+alert(name + "ordered " + quantity + productTitle + " on XYZ clothing store");
